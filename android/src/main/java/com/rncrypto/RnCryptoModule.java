c1252383dc97694c913431e3b20cb171ebc66f5d
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-02-01 18:21:14 +0100

    refactor package entrypoint using repositories

--- android/src/main/java/com/rncrypto/RnCryptoModule.java
+++ android/src/main/java/com/rncrypto/RnCryptoModule.java
@@ -1,15 +1,9 @@
 package com.rncrypto;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.security.keystore.KeyGenParameterSpec;
 
 import androidx.annotation.NonNull;
-import androidx.security.crypto.*;
 
-import com.facebook.common.util.Hex;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
@@ -21,24 +15,6 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 @ReactModule(name = RnCryptoModule.NAME)
 public class RnCryptoModule extends ReactContextBaseJavaModule {
@@ -52,39 +28,6 @@ public class RnCryptoModule extends ReactContextBaseJavaModule {
     @NonNull
     public String getName() {
         return NAME;
-    }
-
-    /*
-     * Convert hex string to byte[]
-     *
-       * @param hexString
-     *            the hex string
-     * @return byte[]
-     */
-    public static byte[] hexStringToBytes(String hexString) {
-      if (hexString == null || hexString.equals("")) {
-        return null;
-      }
-      hexString = hexString.toUpperCase();
-      int length = hexString.length() / 2;
-      char[] hexChars = hexString.toCharArray();
-      byte[] d = new byte[length];
-      for (int i = 0; i < length; i++) {
-        int pos = i * 2;
-        d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
-      }
-      return d;
-    }
-
-    /**
-     * Convert char to byte
-     *
-     * @param c
-     *            char
-     * @return byte
-     */
-    private static byte charToByte(char c) {
-      return (byte) "0123456789ABCDEF".indexOf(c);
     }
 
     @ReactMethod
@@ -134,17 +77,14 @@ public class RnCryptoModule extends ReactContextBaseJavaModule {
       }
     }
 
-    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
-    public static String bytesToHex(byte[] bytes) {
-      byte[] hexChars = new byte[bytes.length * 2];
-      for (int j = 0; j < bytes.length; j++) {
-        int v = bytes[j] & 0xFF;
-        hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-        hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-      }
-      return new String(hexChars, StandardCharsets.UTF_8);
-    }
-
+  /**
+   * Encrypts a file in background.
+   * @param sourcePath Path where file is located
+   * @param destinationPath Path where encrypted file is going to be written
+   * @param hexKey Encryption key in hex format
+   * @param hexIv Initialization vector in hex format
+   * @param cb Only error callback
+   */
     @ReactMethod
     public void encryptFile(
       String sourcePath,
@@ -153,71 +93,32 @@ public class RnCryptoModule extends ReactContextBaseJavaModule {
       String hexIv,
       Callback cb
     ) {
-      Exception ex = null;
-      try {
-        _encryptFile(sourcePath, destinationPath, hexKey, hexIv);
-      } catch (IOException e) {
-        e.printStackTrace();
-        ex = e;
-      } catch (NoSuchPaddingException e) {
-        e.printStackTrace();
-        ex = e;
-      } catch (NoSuchAlgorithmException e) {
-        e.printStackTrace();
-        ex = e;
-      } catch (InvalidAlgorithmParameterException e) {
-        e.printStackTrace();
-        ex = e;
-      } catch (InvalidKeyException e) {
-        e.printStackTrace();
-        ex = e;
-      }
-      if (ex != null) {
-        cb.invoke(ex);
-      } else {
-        cb.invoke((Object) null);
-      }
+      ThreadPerTaskExecutor executor = new ThreadPerTaskExecutor();
+      EncryptFileRepository encryptFileRepository = new EncryptFileRepository(executor);
+
+      encryptFileRepository.encryptFileInBackground(
+        sourcePath,
+        destinationPath,
+        hexKey,
+        hexIv,
+        (Exception ex) -> {
+          if (ex == null) {
+            cb.invoke((Object) null);
+          } else {
+            cb.invoke(ex);
+          }
+        }
+      );
     }
 
-    public void _encryptFile(
-      String sourcePath,
-      String destinationPath,
-      String hexKey,
-      String hexIv
-    ) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
-      FileInputStream fis = new FileInputStream(sourcePath);
-      FileOutputStream fos = new FileOutputStream(destinationPath);
-
-      // byte[] key = new BigInteger("7F" + hexKey, 16).toByteArray();
-      // SecretKeySpec secretKey = new SecretKeySpec(key, 1, key.length-1, "AES");
-
-      byte[] key = Hex.decodeHex(hexKey);
-      byte[] iv = Hex.decodeHex(hexIv);
-
-      SecretKeySpec secretKey = new SecretKeySpec(key, 0, key.length, "AES");
-      // new SecureRandom().nextBytes(iv);
-
-      Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-
-      if (!cipher.getAlgorithm().toUpperCase().startsWith(("AES/CTR")))
-        throw new IllegalArgumentException("Invalid algorithm, only AES/CTR mode supported");
-
-      cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
-
-      CipherOutputStream cos = new CipherOutputStream(fos, cipher);
-
-      int b;
-      byte[] buffer = new byte[4096];
-
-      while ((b = fis.read(buffer)) != -1) {
-        cos.write(buffer, 0, b);
-      }
-
-      cos.flush();
-      cos.close();
-      fis.close();
-    }
-
+  /**
+   * Decrypts a file in background.
+   * @param sourcePath Path where encrypted file is located
+   * @param destinationPath Path where decrypted file is going to be written
+   * @param hexKey Decryption key in hex format
+   * @param hexIv Initialization vector in hex format
+   * @param cb Only error callback
+   */
     @ReactMethod
     public void decryptFile(
       String sourcePath,
@@ -226,65 +127,23 @@ public class RnCryptoModule extends ReactContextBaseJavaModule {
       String hexIv,
       Callback cb
     ) {
-      Exception ex = null;
-      try {
-        _decryptFile(sourcePath, destinationPath, hexKey, hexIv);
-      } catch (InvalidAlgorithmParameterException e) {
-        e.printStackTrace();
-      } catch (NoSuchPaddingException e) {
-        e.printStackTrace();
-        ex = e;
-      } catch (IOException e) {
-        e.printStackTrace();
-        ex = e;
-      } catch (NoSuchAlgorithmException e) {
-        e.printStackTrace();
-        ex = e;
-      } catch (InvalidKeyException e) {
-        e.printStackTrace();
-        ex = e;
-      }
+      ThreadPerTaskExecutor executor = new ThreadPerTaskExecutor();
+      DecryptFileRepository decryptFileRepository = new DecryptFileRepository(executor);
 
-      if (ex != null) {
-        cb.invoke(ex);
-      } else {
-        cb.invoke((Object) null);
-      }
+      decryptFileRepository.decryptFileInBackground(
+        sourcePath,
+        destinationPath,
+        hexKey,
+        hexIv,
+        (Exception ex) -> {
+          if (ex == null) {
+            cb.invoke((Object) null);
+          } else {
+            cb.invoke(ex);
+          }
+        }
+      );
     }
-
-    public void _decryptFile(
-      String sourcePath,
-      String destinationPath,
-      String hexKey,
-      String hexIv
-    ) throws IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
-      FileInputStream fis = new FileInputStream(sourcePath);
-      FileOutputStream fos = new FileOutputStream(destinationPath);
-
-      byte[] key = Hex.decodeHex(hexKey);
-      byte[] iv = Hex.decodeHex(hexIv);
-
-      SecretKeySpec secretKey = new SecretKeySpec(key, 0, key.length, "AES");
-
-      Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-
-      if (!cipher.getAlgorithm().toUpperCase().startsWith(("AES/CTR"))) {
-        throw new IllegalArgumentException("Invalid algorithm, only AES/CTR mode supported");
-      }
-
-      cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
-      CipherInputStream cis = new CipherInputStream(fis, cipher);
-
-      int b;
-      byte[] buffer = new byte[4096];
-      while ((b = cis.read(buffer)) != -1) {
-        fos.write(buffer, 0, b);
-      }
-      fos.flush();
-      fos.close();
-      cis.close();
-    }
-
 
     // Example method
     // See https://reactnative.dev/docs/native-modules-android
