c5f0d25b1a340b6588ac597b55dee05e712eafa4
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-02-01 23:27:14 +0100

    (refactor): decouple encryption steps
    - Decouples encryption step from writing on file system
    - Provides a method for a generic encryption

--- android/src/main/java/com/rncrypto/EncryptFileRepository.java
+++ android/src/main/java/com/rncrypto/EncryptFileRepository.java
@@ -1,10 +1,12 @@
 package com.rncrypto;
 
-import com.facebook.common.util.Hex;
+import com.rncrypto.util.OnlyErrorCallback;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -16,10 +18,6 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-interface RepositoryCallback {
-  void onComplete(Exception ex);
-}
-
 public class EncryptFileRepository {
   private final Executor executor;
 
@@ -30,36 +28,39 @@ public class EncryptFileRepository {
   public void encryptFileInBackground(
     String sourcePath,
     String destinationPath,
-    String hexKey,
-    String hexIv,
-    final RepositoryCallback callback
+    byte[] key,
+    byte[] iv,
+    final OnlyErrorCallback callback
   ) {
-    executor.execute(() -> {
-      try {
-        encryptFile(sourcePath, destinationPath, hexKey, hexIv);
-        callback.onComplete(null);
-      } catch (IOException | NoSuchPaddingException | InvalidAlgorithmParameterException | NoSuchAlgorithmException | InvalidKeyException e) {
-        e.printStackTrace();
-        callback.onComplete(e);
-      }
-    });
+    executor.execute(() -> encryptFile(sourcePath, destinationPath, key, iv, callback));
   }
 
   public void encryptFile(
     String sourcePath,
     String destinationPath,
-    String hexKey,
-    String hexIv
-  ) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
-    FileInputStream fis = new FileInputStream(sourcePath);
-    FileOutputStream fos = new FileOutputStream(destinationPath);
+    byte[] key,
+    byte[] iv,
+    final OnlyErrorCallback onlyErrorCallback
+  ) {
+    try {
+      this.encryptFile(sourcePath, destinationPath, this.getAES256CTRCipher(key, iv));
 
-    byte[] key = Hex.decodeHex(hexKey);
-    byte[] iv = Hex.decodeHex(hexIv);
+      onlyErrorCallback.onComplete(null);
+    } catch (IOException | NoSuchPaddingException | InvalidAlgorithmParameterException | NoSuchAlgorithmException | InvalidKeyException e) {
+      e.printStackTrace();
+      onlyErrorCallback.onComplete(e);
+    }
+  }
 
+  /**
+   * Generates an AES-256-CTR Cipher
+   *
+   * @param key Cipher key
+   * @param iv Initialization vector
+   * @return Cipher used for encrypting with AES-256-CTR
+   */
+  private Cipher getAES256CTRCipher(byte[] key, byte[] iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
     SecretKeySpec secretKey = new SecretKeySpec(key, 0, key.length, "AES");
-    // new SecureRandom().nextBytes(iv);
-
     Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
 
     if (!cipher.getAlgorithm().toUpperCase().startsWith(("AES/CTR")))
@@ -67,17 +68,51 @@ public class EncryptFileRepository {
 
     cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
 
-    CipherOutputStream cos = new CipherOutputStream(fos, cipher);
+    return cipher;
+  }
+
+  /**
+   * Encrypts a file at sourcePath, writes the output to destinationPath
+   *
+   * @param sourcePath      Path where file to encrypt is
+   * @param destinationPath Path where encrypted content is going to be written
+   * @param cipher          Cipher used to encrypt the content
+   */
+  private void encryptFile(
+    String sourcePath,
+    String destinationPath,
+    Cipher cipher
+  ) throws IOException {
+    this.encrypt(
+      new FileInputStream(sourcePath),
+      new FileOutputStream(destinationPath),
+      cipher
+    );
+  }
+
+  /**
+   * Encrypts content received from an input and writes it to an output
+   *
+   * @param inputStream  Source to encrypt
+   * @param outputStream Source to write encrypted content
+   * @param cipher       Cipher used to encrypt data
+   */
+  public void encrypt(
+    InputStream inputStream,
+    OutputStream outputStream,
+    Cipher cipher
+  ) throws IOException {
+    CipherOutputStream cos = new CipherOutputStream(outputStream, cipher);
 
     int b;
     byte[] buffer = new byte[4096];
 
-    while ((b = fis.read(buffer)) != -1) {
+    while ((b = inputStream.read(buffer)) != -1) {
       cos.write(buffer, 0, b);
     }
 
     cos.flush();
     cos.close();
-    fis.close();
+    inputStream.close();
   }
 }
