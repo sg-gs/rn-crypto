52bb15a49b67e7858a7b57b0ce106b2d7fd38191
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-02-01 18:20:32 +0100

    add ThreadPerTaskExecutor.java

---
+++ android/src/main/java/com/rncrypto/ThreadPerTaskExecutor.java
@@ -0,0 +1,9 @@
+package com.rncrypto;
+
+import java.util.concurrent.Executor;
+
+public class ThreadPerTaskExecutor implements Executor {
+  public void execute(Runnable r) {
+    new Thread(r).start();
+  }
+}
